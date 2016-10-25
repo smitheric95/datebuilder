f07fcfd4c6f7ce41c105444df3a01e30d5d18d40
Jake Carlson <jrcarlson@smu.edu>
2016-10-25 11:28:42 -0500

    Slim Skeleton
    Installed the slim skeleton.

---
+++ datebuilder/src/routes.php
@@ -0,0 +1,25 @@
+<?php
+// Routes
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+
+$app = new \Slim\App;
+
+$app->get('/[{name}]', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+// $app->run();
