e0e60aeff601cc1590abff3bb04e1ba579572076
Eric Smith <ericsmith@Eric-Smiths-MacBook.local>
2016-10-27 19:35:21 -0500

    slim landing page

--- datebuilder/src/routes.php
+++ datebuilder/src/routes.php
@@ -4,16 +4,17 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-// require '../vendor/autoload.php';
+require '../vendor/autoload.php';
 
 $app = new \Slim\App;
 
 $app->get('/[{name}]', function ($request, $response, $args) {
     // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+    // $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+    $view = new \Slim\Views\PhpRenderer(__DIR__.'/../templates/');
+    return $view->render($response, './index.phtml', $args);
 });
 
 $app->get('/hello/{name}', function (Request $request, Response $response) {
