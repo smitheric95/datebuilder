3f87eb5cbedc98356d2f4ad2462b6ac852a339fe
Henry Warren <henrysdev@gmail.com>
2016-11-02 14:28:15 -0500

    date elements

--- datebuilder/src/build_date.php
+++ datebuilder/src/build_date.php
@@ -10,48 +10,16 @@ function build_date($business, $distances, $categories, $total_cost, $name, $tot
     $db_password = "pass";
     $table_name = "datebuilder_db.dates";
     $subtable_name = "datebuilder_db.date_elements";
-
     $conn = new mysqli($db_servername, $db_username, $db_password);
 
     // Check connection
     if ($conn->connect_error) {
-//        echo "Connection failed: " . $conn->connect_error;
+        echo "Connection failed: " . $conn->connect_error;
 //        $logger->error('Connection to database failed: ', $conn->connect_error);
     }
-
     // regex check input
 
-    for($i = 0; $i < sizeof($business); $i++)
-    {
-        $pattern = "/\w+/";
-        $match = preg_match($pattern, $business[$i]);
-        if ($match != 1)
-        {
-            $conn->close();
-            return "Invalid 'business' input";
-        }
-    }
-    for($i = 0; $i < sizeof($distances); $i++)
-    {
-        $pattern = "/\d+.\d*/";
-        $match = preg_match($pattern, $distances[$i]);
-        if ($match != 1)
-        {
-            $conn->close();
-            return "Invalid 'distances' input";
-        }
-    }
-    #for($i = 0; $i < sizeof($categories); $i++)
-    #{
-    #    $pattern = "/\d+.\d*/";
-    #    $match = preg_match($pattern, $categories[$i]);
-    #    if ($match != 1)
-    #    {
-    #        $conn->close();
-    #        return "Invalid 'categories' input";
-    #    }
-    #}
-    
+    # in progress (regex need to be much stricter)  
     $total_cost_pattern = "/\d+(.\d+)?/";
     $name_pattern = "/\w+/";
     $total_time_pattern = "/\d+/";
@@ -63,15 +31,32 @@ function build_date($business, $distances, $categories, $total_cost, $name, $tot
     $iu_match = preg_match($image_url_pattern, $image_url);
 
     // if the input fields are all valid, the user is added to the users table
-    if ($tc_match == 1 && $n_match == 1 && $tt_match == 1 && $iu_match == 1*/) {
+    if (1 < 2){#$tc_match == 1 && $n_match == 1 && $tt_match == 1 && $iu_match == 1) {
 
 //        $email = mysqli_real_escape_string()
+        for($i = 0; $i < sizeof($business); $i++)
+        {
+            $this_business = $business[$i];
+            $sql = "INSERT INTO {$subtable_name} (business_id) VALUES ('$this_business')";
+            if ($conn->query($sql) === TRUE) {
+                echo $sql;
+                echo "successfully posted to date_elements  ";
+            }  
+            else
+            {
+                echo("Error description: " . mysqli_error($conn));
+                return "error creating date elements";
+            }
+            #else{
+            #    return "Error creating date_elements table";
+            #}
+        }  
+        
 
         $sql = "INSERT INTO {$table_name} (date_id, user_id, name, total_cost, total_time, image_url) VALUES (NULL,1, '$name', '$total_cost', '$total_time', '$image_url')";
         #(in progress -> posting date elements)  "INSERT INTO {$subtable_name} (de_id, date_id, business_id)";
 
         if ($conn->query($sql) === TRUE) {
-            echo "New user successfully created";
             $conn->close();
             return TRUE;
         } else {
--- datebuilder/src/routes.php
+++ datebuilder/src/routes.php
@@ -51,25 +51,22 @@ $app->post('/users/', function (Request $request, Response $response) {
 $app->post('/build/', function (Request $request, Response $response) {
 
     $parsed_body = $request->getParsedBody();
-    var_dump( $parsed_body);
+    var_dump($parsed_body); 
     $business = $parsed_body['business'];
     $distances = $parsed_body['distances'];
     $categories = $parsed_body['categories'];
     $total_cost = $parsed_body['total_cost'];
     $name = $parsed_body['name'];
     $total_time = $parsed_body['total_time'];
-    $image_url = ['image_url'];
-
+    $image_url = $parsed_body['image_url'];
     include 'build_date.php';
 
     $status = build_date($business, $distances, $categories, $total_cost, $name, $total_time, $image_url);
-
     if ($status === TRUE) {
         $response->getBody()->write("Date built!!!");
     } else {
         $response->getBody()->write("Error building date: ". $status);
     }
-    
     return $response;
 });
 
