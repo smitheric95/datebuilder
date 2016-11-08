f6333ac040f7f0b33d44ae26b7912a15856d5ef6
Eric Smith <eric@smu.edu>
2016-11-07 20:19:45 -0600

    sign up form

---
+++ datebuilder/public/app/account/account.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'account',
+    templateUrl: './app/account/account.html',
+    styleUrls: ['./app/account/account.css']
+})
+export class AccountComponent {
+    
+}
