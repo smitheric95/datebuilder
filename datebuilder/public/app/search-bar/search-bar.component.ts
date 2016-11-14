47b7826448d165769b81de4db22ef6bb9fd89b9a
Eric Smith <eric@smu.edu>
2016-11-14 14:49:09 -0600

    searchbar bootstrap

--- datebuilder/public/app/search-bar/search-bar.component.ts
+++ datebuilder/public/app/search-bar/search-bar.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'search-bar',
-    templateUrl: './app/search-bar/search-bar.html'
+    templateUrl: './app/search-bar/search-bar.html',
+    styleUrls: [
+        './node_modules/bootstrap/dist/css/bootstrap.min.css',
+        './node_modules/bootstrap/node_modules/bootstrap-material-design/dist/css/bootstrap-material-design.min.css'
+    ]
 })
 export class SearchbarComponent{
     
--- datebuilder/public/app/search/search.component.ts
+++ datebuilder/public/app/search/search.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'search',
     templateUrl: './app/search/search.html',
-    styleUrls: ['./app/search/search.css']
+    styleUrls: [
+        './app/search/search.css',
+        './node_modules/bootstrap/dist/css/bootstrap.min.css',
+        './node_modules/bootstrap/node_modules/bootstrap-material-design/dist/css/bootstrap-material-design.min.css'
+    ]
 })
 export class SearchComponent {
 
